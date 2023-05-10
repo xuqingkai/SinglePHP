ea5acaf440aeb5a1ac3958929e1ab20c3c6aee92
xuqingkai <id@xuqingkai.com>
2023-05-10 15:35:45 +0800

    修改了一处代码，使得入口文件index.php可以从public目录迁移到根目录，方便部分虚拟主机用户无法指定子目录为根目录

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+define('APP_DEBUG', TRUE);
+require './SinglePHP.php';                //采用单独文件部署方式
+
+use SinglePHP\Config;
+use SinglePHP\SinglePHP;
+
+SinglePHP::getInstance(Config::Init())->run();     //跑起来啦
